0dfaca597581a67469f201a560349bcbad936675
Nicholas Walter <nicholasalexanderwalter@gmail.com>
2017-07-25 20:55:32 -0600

    Refactored for appJar over Kivy
    No mobile/web support now - but the GUI is functional. Story of my life, innit.
    
    Double-clicking on run.bat runs the program, minimizing the cmdline and opening up the GUI. Looks pretty good so far, all things considered.

--- gui.py
+++ gui.py
@@ -1,19 +1,47 @@
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
+from appJar import gui
 
+#initializing application
+app = gui()
 
-class IntroScreen(GridLayout):
-    def __init__(self, **kwargs):
-        super(IntroScreen, self).__init__(**kwargs)
-        self.add_widget(Label(text='Welcome to Nicholas Walter\'s D&D Utility Tool!'))
+app.setTitle("D&D Utility Tool")
+app.setGeometry("300x300")
+
+#setting up widgets
+fList = ["NPCs", "Buildings", "Village", "Rumors", "Encounters", "Loot", "XP Budget Calculator", "Combat Encounter Generator", "Monster Lists", "Wild Magical Effect", "Print a List", "Spell Cards"]
+app.addListBox("functionsList", fList)
+app.setListBoxRows("functionsList", len(fList))
+
+#setting up basic functionality - via "Proceed" button
+choice = 0
+
+def proceed(choice):
+	choice_str = app.getListItems("functionsList")[0]
+	if choice_str == "NPCs":
+		choice = 1
+	elif choice_str == "Buildings":
+		choice = 2
+	elif choice_str == "Village":
+		choice = 3
+	elif choice_str == "Rumors":
+		choice = 4
+	elif choice_str == "Encounters":
+		choice = 5
+	elif choice_str == "Loot":
+		choice = 6
+	elif choice_str == "XP Budget Calculator":
+		choice = 7
+	elif choice_str == "Combat Encounter Generator":
+		choice = 8
+	elif choice_str == "Monster Lists":
+		choice = 9
+	elif choice_str == "Wild Magical Effect":
+		choice = 10
+	elif choice_str == "Print a List":
+		choice = 11
+	elif choice_str == "Spell Cards":
+		choice = 12
 		
+app.addButton("Proceed", proceed)
 
-		
-class MyApp(App):
-    def build(self):
-        return IntroScreen()
-
-
-if __name__ == '__main__':
-    MyApp().run()
+#starts app
+app.go()
