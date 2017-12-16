4aa9ec858db48d673bdfe86c8a4f4f3cfc8c2057
Nicholas Walter <nickwalt@rams.colostate.edu>
2017-12-16 10:36:52 -0700

    fixed design, implementing some stubs, moved files

---
+++ Java/DungeonsAndDragonsUtilityTool/src/Creature.java
@@ -0,0 +1,7 @@
+public class Creature {
+	
+	public Creature() {
+		
+	}
+	
+}
--- Java/DungeonsAndDragonsUtilityTool/src/Model.java
+++ Java/DungeonsAndDragonsUtilityTool/src/Model.java
@@ -1,4 +1,72 @@
+import java.util.ArrayList;
 
 public class Model {
-
+	
+	ArrayList<Creature> Creatures = new ArrayList<Creature>(500);
+	
+	public Model() {
+		Creatures = Model.loadCreatures();
+	}
+	
+	/**
+	 * 
+	 * @param numberDice
+	 * @param numberSides
+	 * @return
+	 */
+	public static int makeRoll(int numberDice, int numberSides) {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param roll
+	 * @return
+	 */
+	public static int roll(String roll) {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static ArrayList<Creature> loadCreatures(){
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param numberPlayers
+	 * @param averageLevel
+	 * @return
+	 */
+	public static int calculateXP(int numberPlayers, int averageLevel) {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param attributes
+	 * @return
+	 */
+	public ArrayList<Creature> searchCreatures(ArrayList<String> attributes) {
+		return null;
+	}
+	
+	
+	/**
+	 * 
+	 * @param numberCreatures
+	 * @param numberBosses
+	 * @param numberMinions
+	 * @param xpBudget
+	 * @param attributes
+	 * @return
+	 */
+	public ArrayList<Creature> generateEncounter(int numberCreatures, int numberBosses, int numberMinions, int xpBudget, ArrayList<String> attributes) {
+		return null;
+	}
+	
+	
 }
