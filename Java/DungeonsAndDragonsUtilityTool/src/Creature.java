4532ed636015f9c093b352e656a9e3dfe3788dd6
Nicholas Walter <nickwalt@rams.colostate.edu>
2017-12-16 10:58:04 -0700

    data objects

--- Java/DungeonsAndDragonsUtilityTool/src/Creature.java
+++ Java/DungeonsAndDragonsUtilityTool/src/Creature.java
@@ -1,7 +1,18 @@
 public class Creature {
 	
-	public Creature() {
-		
-	}
+	String environment = null;
+	String name = null;
+	String type = null;
+	int xp = -1;
+	String book = null;
+	int pageNumber = -1;
 	
+	public Creature(String environment, String name, String type, int xp, String book, int page) {
+		this.environment = environment;
+		this.name = name;
+		this.type = type;
+		this.xp = xp;
+		this.book = book;
+		this.pageNumber = page;
+	}
 }
---
+++ Java/DungeonsAndDragonsUtilityTool/src/Roll.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Roll {
+	
+	public ArrayList<Integer> sortedRolls = null;
+	public int total = 0;
+	
+	public Roll(ArrayList<Integer> sortedRolls, int total) {
+		this.sortedRolls = sortedRolls;
+		this.total = total;
+	}
+}
