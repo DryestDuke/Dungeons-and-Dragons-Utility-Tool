b418ecd2c6e2696e4b1bd5d0945aabddce7dc9bb
Nicholas Walter <nickwalt@rams.colostate.edu>
2017-12-17 21:26:34 -0600

    .

--- Java/DungeonsAndDragonsUtilityTool/src/NPCScreen.java
+++ Java/DungeonsAndDragonsUtilityTool/src/NPCScreen.java
@@ -5,6 +5,10 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JSeparator;
 
 public class NPCScreen extends JFrame {
 
@@ -37,11 +41,26 @@ public class NPCScreen extends JFrame {
 		setTitle("NPCs");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Dashboard.class.getResource("/com/jtattoo/plaf/icons/empty_8x8.png")));
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 600, 300);
+		setBounds(100, 100, 600, 569);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Saved NPCs");
+		lblNewLabel.setFont(new Font("Courier New", Font.PLAIN, 14));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(98, 12, 80, 17);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Generate an NPC");
+		lblNewLabel_1.setFont(new Font("Courier New", Font.PLAIN, 14));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(378, 12, 120, 17);
+		contentPane.add(lblNewLabel_1);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(285, 12, 1, 515);
+		contentPane.add(separator);
 	}
-
 }
