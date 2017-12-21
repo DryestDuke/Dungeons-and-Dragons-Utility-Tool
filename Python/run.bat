5329a1e23d5777850679d0f6684c3b5b7818ad95
Nicholas Walter <nickwalt@rams.colostate.edu>
2017-12-20 21:34:07 -0600

    Removed Python.
    As a result, the Java project is now the top level of the repository.

--- Python/run.bat
+++
@@ -1,7 +0,0 @@
-REM code from rbprogrammer at [stackoverflow.com/questions/2106723/how-to-minimize-the-command-prompt-when-running-a-batch-script]
-if not "%minimized%"=="" goto :minimized
-set minimized=true
-start /min cmd /C "%~dpnx0"
-goto :EOF
-:minimized
-c:\Users\nicho\AppData\Local\Programs\Python\Python36-32\python gui.py
